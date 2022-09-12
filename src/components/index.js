697955dd78e35dfd5f65fd918225f84959b59922
nicofrancis <nicolofrancis10@gmail.com>
2022-09-12 14:05:29 -0600

    responsive navbar working

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ export { default as Button } from "./Button";
 export { default as Contact } from "./Contact";
 export { default as Footer } from "./Footer";
 export { default as Hero } from "./Hero";
-export { default as NavBar } from "./NavBar";
+export { default as Navbar } from "./Navbar";
 export { default as Projects } from "./Projects";
 export { default as Skills } from "./Skills";
 export { default as ViewProjects } from "./ViewProjects";
