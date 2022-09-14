9a7723a3ed1b93f61bffb2c7911fa335f622d53f
nicofrancis <nicolofrancis10@gmail.com>
2022-09-13 20:03:54 -0600

    contact card coming together

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import styles from "../style";
 function Contact() {
-  return <div id="contact">Contact</div>;
+  return (
+    <div
+      id="contact"
+      className="w-full h-screen flex justify-center items-center p-4"
+    >
+      <form
+        method="POST"
+        action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c"
+        className="flex flex-col max-w-[600px] w-full"
+      >
+        <div className="pb-8">
+          <h1
+            className={`flex-2 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] ${styles.flexCenter}`}
+          >
+            Contact
+          </h1>
+        </div>
+
+        <input className="p-2" type="text" placeholder="Name" name="name" />
+        <input
+          className="my-4 p-2"
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default Contact;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,18 @@
 import styles from "../style";
 import { logo } from "../assets";
-import ViewProjects from "./ViewProjects";
 
 const Hero = () => (
   <section id="home" className={`flex md:flex-row flex-col ${styles.paddingX}`}>
     <div
-      className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+      className={`${styles.flexStart} flex-1 flex-col xl:px-0 sm:px-16 px-6`}
     >
       <div className="flex flex-row justify-between items-center w-full">
         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
           <span>I'm Nico</span>
           <br className="sm:block hidden" />{" "}
-          <span className="text-gradient">I build things for the web.</span>
+          <span className="text-gradient">I build things</span>
+          <br className="sm:block hidden" />
+          <span className="text-gradient">for the web.</span>
         </h1>
       </div>
 
@@ -19,9 +20,6 @@ const Hero = () => (
         I’m a fullstack software engineer specializing in building exceptional
         digital experiences. Here to turn your ideas into reality.
       </p>
-      <div className={` ${styles.flexCenter}`}>
-        <ViewProjects />
-      </div>
     </div>
 
     <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
@@ -30,10 +28,6 @@ const Hero = () => (
       <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
       <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
       <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
-    </div>
-
-    <div className={`ss:hidden ${styles.flexCenter}`}>
-      <ViewProjects />
     </div>
   </section>
 );
